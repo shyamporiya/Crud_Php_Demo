7184828329a4d322577bf833ca2c27dd02e02bb2
Shyamporiya <shyamporiya9595@gmail.com>
2023-10-14 00:46:18 +0530

    set cookies functionality on login page

--- login.php
+++ login.php
@@ -1,40 +1,91 @@
-<?php
-// echo "<pre>";print_r($_POST);die;
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "data_structure";
-// Create connection
-
-$conn = mysqli_connect($servername,$username,$password,$database);
-
-$mobile = $_POST['mobile'];
-$password = $_POST['password'];
-
-$sql = "SELECT * FROM `sign_ups` WHERE `mobile` = '".$_POST['mobile']."' AND `password` = '".$_POST['password']."' ";
-$result = mysqli_query($conn,$sql);
-if(mysqli_num_rows($result) == 1){
-$name = "SELECT fname FROM  `sign_ups` WHERE `mobile` = ".$mobile." ";
-$result1 = mysqli_query($conn,$name);
-$name = mysqli_fetch_assoc($result1);
-$username = $name['fname'];
-// echo "<pre>";print_r($name['fname']);die;
-
-
-    // echo $name;die;
-    // echo "Login Successfully";
-    session_start();
-    $_SESSION['message'] = "Welcome $username ";
-    header("Location:index.php");
-}else if(mysqli_num_rows($result) == 0){
-    session_start();
-    $_SESSION['message'] = 'No User Found';
-    header("Location:sign-in.php");
-    // echo "No Record Found";
-}else{
-    echo "Something Went Wrong";
+<?php include "header.php"; ?>
+<style>
+  body{
+    background-color:#233140 !important;
+    
 }
-// $data = mysqli_fetch_assoc($result);
-// // if($data['mobile'] == $_POST['mobile']){}
-?>
+</style>
+    <section class="main_section">
+  <!-- <a href="create_account.php" target="_blank" style="border: 2px solid red;">Form</a>
+  <a href="table.php">Table</a> -->
+
+        
+        <div class="container" style="margin-top: 150px;">
+          <div class="container center_form">
+              <form id="login-form" action="sign-in.php" method="POST" enctype="multipart/form-data">
+                <?php 
+                session_start();
+
+                if (isset($_SESSION['message'])) {?>
+                <div class="text-center" style="background-color: red;">
+
+                  <h5><b><?php echo $_SESSION['message']; ?> </b></h5>
+                </div>
+        <?php  unset($_SESSION['message']); } ?>
+                <h3><b>Login Account</b></h3>
+                <div class="row">
+                  <div class="col-md-12 mb-2">
+                    <div class="">
+                    <label class="form-label">Mobile No</label>
+                      <input type="text" id="mobile" name="mobile" class="form-control" value="<?php if(isset($_COOKIE['mobile'])) { echo $_COOKIE['mobile'];} ?>"/>
+                    </div>
+                  </div>
+                  <div class="col-md-12 mb-2">
+                    <div class="form-outline">
+                    <label class="form-label">Password</label>
+                      <input type="password" id="password" name="password" class="form-control" value="<?php if(isset($_COOKIE['password'])) { echo $_COOKIE['password'];} ?>"/>
+                    </div>
+                  </div>
+                  <div>
+                    <input type="checkbox" name="remember" id="remember">
+                    <label>Remember Me</label>            
+              
+
+                  </div>
+                <div class="pt-3">
+                  <button type="submit" id="submit" class="btn btn-warning sing-in-btn">Login</button>
+                  <a type="button" class="btn btn-warning sing-in-btn">Forgot Password</a>
+                </div>
+                </form>
+    
+                <div class="pt-3">
+                  <span class="btn sign-in-btn bg-grey" style="color:yellow">Don't Have a Account</span>
+                  <a href="create_account.php" class="btn btn-light sign-in-btn">Sign Up</a>
+                </div>
+
+                
+            
+            
+
+        </div>
+      </div>
+      </section>
+  <?php include "footer.php"; ?>
+
+<script>
+    $(document).ready(function () {
+        $("#login-form").validate({
+            rules: {
+                mobile: {
+                    required: true,
+                    minlength: 4,
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                },
+            },
+            messages: {
+                mobile: {
+                    required: "Please enter your username",
+                    minlength: "Username must be at least 4 characters long",
+                },
+                password: {
+                    required: "Please enter your password",
+                    minlength: "Password must be at least 6 characters long",
+                },
+            },
+        });
+    });
+</script>
+
